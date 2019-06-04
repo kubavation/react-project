7a1b9cd5c7551a8e52a6dc8455fa1434bac2a87e
cichybiala <40429973+cichybiala@users.noreply.github.com>
2019-06-04 17:29:08 +0200

    Dodany 4 wykres
    :ant:

--- first-app/src/components/charts/charts.js
+++ first-app/src/components/charts/charts.js
@@ -8,9 +8,10 @@ import {withStyles} from "@material-ui/core/styles/index";
 import Paper from "@material-ui/core/Paper";
 import Button from '@material-ui/core/Button';
 
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
-const COLORSCo2 = ['F98866', '#89DA59', '#FF420E', '#80BD9E'];
-const COLORSWater = ['#505160', '#598234', '#68829E', '#AEBD38'];
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042','#CCFF66','#5D2E8C','#2EC4B6','#F1E8B8','#FF6666'];
+const Colors = ['#A4036F', '#048BA8','#16DB93','#EFEA5A','#F29E4C','#C89933','#DBD053','#74526C','#5C415D']
+const Colors2 = [ '#4B4237','#D5A021','#EDE7D9','#A49694','#736B60', '#212D40','#11151C', '#364156', '#7D4E57', '#D66853'];
+const Colors3 = ['#020122','#F4442E','#FC9E4F','#EDD382','#F2F3AE','#FBBFCA','#FFEED6','#D2BF55','#875053','#2B061E'];
 
 
 const styles = theme => ({
@@ -129,6 +130,8 @@ class Charts extends Component {
             chartEqco2: [],
             chartProducts: [],
             chartWater: [],
+            chartCO2: [],
+            output: [],
             json: JSON.stringify(chartIn)
         }
        // this.getCharts(props);
@@ -253,76 +256,6 @@ class Charts extends Component {
     }*/
     getCharts(input){
         console.log("GET_CHARTS")
-        // let chartIn = {
-        //     energy_resources: [
-        //         {
-        //             gus: "060",
-        //             name: "Węgiel kamienny energetyczny z wyłączeniem brykietów",
-        //             unit: "tona",
-        //             ammount: 1234.2
-        //         },
-        //         {
-        //             gus: "088",
-        //             name: "benzyna silnikowa bezołowiowa",
-        //             unit: "tona",
-        //             ammount: 3214.2
-        //         },
-        //         {
-        //             gus: "024",
-        //             name: "energia elektryczna",
-        //             unit: "MWh",
-        //             ammount: 3122.4
-        //         }
-        //     ],
-        //     resources: [
-        //         {
-        //             res_id: 1,
-        //             name: "WATER",
-        //             unit: "m3",
-        //             ammount: 255255
-        //         },
-        //         {
-        //             res_id: 14,
-        //             name: "BROKUŁ RÓŻA JESIENNY",
-        //             unit: "kg",
-        //             ammount: 1093508
-        //         },
-        //         {
-        //             res_id: 18,
-        //             name: "GROSZEK",
-        //             unit: "kg",
-        //             ammount: 1011485
-        //         }
-        //     ],
-        //     products: [
-        //         {
-        //             prod_id: 11,
-        //             name: "Mrożony groszek",
-        //             unit: "kg",
-        //             ammount: 981100,
-        //             resources: [
-        //                 18, 1
-        //             ],
-        //             energy_resources: [
-        //                 "024"
-        //             ]
-        //         },
-        //         {
-        //             prod_id: 32,
-        //             name: "Mrożony brokuł",
-        //             unit: "kg",
-        //             ammount: 231128,
-        //             resources: [
-        //                 14, 1
-        //             ],
-        //             energy_resources: [
-        //                 "024",
-        //                 "088",
-        //                 "060"
-        //             ]
-        //         }
-        //     ]
-        // };
 
 
         fetch('http://api.gabryelkamil.pl/calc', {
@@ -358,10 +291,21 @@ class Charts extends Component {
                 }
                 chartProducts.push(temp);
             });
-
+            let chartCO2 = [];
+            data.EN.forEach( u => {
+                const temp  = {
+                    name: u.name,
+                    value: u.ammount
+                }
+                chartCO2.push(temp);
+            });
+            let output = JSON.stringify(data);
             this.setState({chartEqco2: chartEqco2});
             this.setState({chartProducts: chartProducts});
             this.setState({chartWater: chartWater});
+            this.setState({chartCO2: chartCO2});
+            this.setState({output: output});
+
 
         });
     }
@@ -370,60 +314,72 @@ class Charts extends Component {
         const chartEqco2 = this.state.chartEqco2;
         const chartProducts = this.state.chartProducts;
         const chartWater = this.state.chartWater;
+        const chartCO2 = this.state.chartCO2;
 
         const { json } = this.state;
-
-        console.log("?")
+        const output = this.state.output;
 
         const {classes} = this.props;
         return (
             <div>
-            <ResponsiveContainer height={400} width="100%">
+            <ResponsiveContainer height={800} width="100%">
                 <PieChart style={{backgroundColor:"#EEE", borderRadius:'25px'}} >
-                    <text x="20%" y="10%" textAnchor="middle" dominantBaseline="middle" style={{fontWeight:'bold'}}>
-                        Emisja CO2
+                    <text x="30%" y="10%" textAnchor="middle" dominantBaseline="middle" style={{fontWeight:'bold'}}>
+                        Emisja CO2[kg]
                     </text>
-                    <Pie dataKey="value" isAnimationActive={false} data={chartEqco2} cx="20%" cy={200} outerRadius={100} fill="#8884d8" label >
+                    <Pie dataKey="value" isAnimationActive={false} data={chartEqco2} cx="30%" cy="32%" outerRadius={100} fill="#8884d8" label >
                         {
-                            chartEqco2.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORSCo2[index % COLORS.length]} />)
+                            chartEqco2.map((entry, index) => <Cell key={`cell-${index}`} fill={Colors2[index % Colors2.length]} />)
                         }
                     </Pie>
-                    <text x="50%" y="10%" textAnchor="middle" dominantBaseline="middle" style={{fontWeight:'bold'}}>
-                        Emisja wody
+                    <text x="70%" y="10%" textAnchor="middle" dominantBaseline="middle" style={{fontWeight:'bold'}}>
+                        Emisja CO2/Zasób energetyczny
                     </text>
-                    <Pie dataKey="value" isAnimationActive={false} data={chartWater} cx="50%" cy={200} outerRadius={100} fill="#8884d8" label >
+                    <Pie dataKey="value" isAnimationActive={false} data={chartCO2} cx="70%" cy="32%" outerRadius={100} fill="#8884d8" label >
                         {
-                            chartWater.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORSWater[index % COLORS.length]} />)
+                            chartCO2.map((entry, index) => <Cell key={`cell-${index}`} fill={Colors[index % Colors.length]} />)
                         }
                     </Pie>
-                    <text x="80%" y="10%" textAnchor="middle" dominantBaseline="middle" style={{fontWeight:'bold'}}>
+                    <text x="30%" y="50%" textAnchor="middle" dominantBaseline="middle" style={{fontWeight:'bold'}}>
+                        Emisja wody[l]
+                    </text>
+                    <Pie dataKey="value" isAnimationActive={false} data={chartWater} cx="30%" cy="72%" outerRadius={100} fill="#8884d8" label >
+                        {
+                            chartWater.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
+                        }
+                    </Pie>
+                    <text x="70%" y="50%" textAnchor="middle" dominantBaseline="middle" style={{fontWeight:'bold'}}>
                         Produkty
                     </text>
-                    <Pie dataKey="value" isAnimationActive={false} data={chartProducts} cx="80%" cy={200} outerRadius={100} fill="#8884d8" label >
+                    <Pie dataKey="value" isAnimationActive={false} data={chartProducts} cx="70%" cy="72%" outerRadius={100} fill="#8884d8" label >
                         {
-                            chartProducts.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
+                            chartProducts.map((entry, index) => <Cell key={`cell-${index}`} fill={Colors3[index % Colors3.length]} />)
                         }
                     </Pie>
-
                     <Tooltip />
                     <Legend />
                 </PieChart>
-
-
-
             </ResponsiveContainer>
 
                 <Paper style={{marginLeft: '20%', width: '60%', backgroundColor: '#EEE', borderRadius: '25px'}}>
                     <h1>Wstaw dane</h1>
                     <br/>
                         <TextField id="desc" label="JSON" variant="outlined"
-                                   style={{width: '60%'}}
+                                   style={{width: '70%'}}
                                    onChange={this.onChange}
                                    margin="normal" value={json}
                                     name="json"
                                    multiline
                                    rows="15"/>
                         <br/>
+                    <TextField id="output" label="JSON Wyjściowy" variant="outlined"
+                               style={{width: '70%'}}
+                               disabled
+                               margin="normal" value={output}
+                               name="output"
+                               multiline
+                               rows="15"/>
+                         <br/>
                         <Button style={{marginBottom: '5%',marginTop:'5%',backgroundColor: "#86C232"}}
                                 variant="contained" color="primary" onClick={e => this.onSubmit(e)}
                                     size="large">
